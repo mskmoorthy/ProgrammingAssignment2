5351eee8131c24c701d7734e131f6b8bdb48d85a
mskmoorthy <mskmoorthy@gmail.com>
2015-07-13 13:20:31 -0400

    Added Cached Matrix Solver by mskmoorthy

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+inVerse <- NULL
+  set <- function(y) {
+    x <<- y
+    inVerse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inVerse <<- inverse
+  getinverse <- function() inVerse
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+inVerse <- x$getinverse()
+  if(!is.null(inVerse)) {
+    message("getting cached data.")
+    return(inVerse)
+  }
+  data <- x$get()
+  inVerse <- solve(data)
+  x$setinverse(inVerse)
+  inVerse
+
 }
